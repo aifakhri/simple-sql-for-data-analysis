0de5544d829afa7676f8eb2db200f88a7d38bfaa
fakhri <fakhri.azhar@proton.me>
2022-11-07 20:46:59 +0700

    Repar rented_film_and_profit query and adding rented_film_by_customer_query

--- sql_query.py
+++ sql_query.py
@@ -4,7 +4,8 @@ SELECT
 	rental.rental_id,
 	film.film_id,
 	film.title,
-	film.rental_rate
+	film.rental_rate,
+	film.replacement_cost
 FROM inventory
 JOIN film ON inventory.film_id = film.film_id
 JOIN rental ON inventory.inventory_id = rental.inventory_id
@@ -12,10 +13,33 @@ JOIN rental ON inventory.inventory_id = rental.inventory_id
 
 SELECT
 	rented_film.title,
+	rented_film.rental_rate,
+	rented_film.replacement_cost,
 	count(rented_film.title) as n_film,
 	sum(payment.amount) as profit_by_film
 FROM payment
 JOIN rented_film USING(rental_id)
 GROUP BY rented_film.title
 ORDER BY rented_film.title
+"""
+
+rented_film_by_customer = """
+WITH rented_film as (
+SELECT
+	rental.customer_id,
+	film.film_id,
+	film.title,
+	film.rental_rate
+FROM inventory
+JOIN film ON inventory.film_id = film.film_id
+JOIN rental ON inventory.inventory_id = rental.inventory_id
+)
+
+SELECT
+	customer.first_name,
+	customer.last_name,
+	count(rented_film.title) as n_film
+FROM customer
+LEFT JOIN rented_film on customer.customer_id = rented_film.customer_id
+GROUP BY customer.first_name, customer.last_name;
 """
